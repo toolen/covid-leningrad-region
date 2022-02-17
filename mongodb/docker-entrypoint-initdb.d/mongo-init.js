72d59b6a08f5082af2229ca32ebfc4ca2b996a84
Dmitrii Zakharov <dmitrii@zakharov.cc>
2022-02-17 15:51:29 +0300

    feat(mongodb): allow to create test database

--- mongodb/docker-entrypoint-initdb.d/mongo-init.js
+++ mongodb/docker-entrypoint-initdb.d/mongo-init.js
@@ -3,7 +3,7 @@ db.getSiblingDB("$external").runCommand(
         createUser: "CN=scraper",
         roles: [
             {role: "readWrite", db: "covid_stat"},
-            {role: "readWrite", db: "covid_stat_test"},
+            {role: "dbOwner", db: "covid_stat_test"},
         ],
         writeConcern: {w: "majority", wtimeout: 5000}
     }
@@ -13,6 +13,7 @@ db.getSiblingDB("$external").runCommand(
         createUser: "CN=dashboard",
         roles: [
             {role: "read", db: "covid_stat"},
+            {role: "dbOwner", db: "covid_stat_test"},
         ]
     }
 )
