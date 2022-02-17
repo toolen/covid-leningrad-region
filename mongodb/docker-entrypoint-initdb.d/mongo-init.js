9bcbe6c154d33832b098afc442c1333d837a0238
Dmitrii Zakharov <dmitrii@zakharov.cc>
2022-02-17 16:29:36 +0300

    feat(mongodb): restore database from dump for development needs

--- mongodb/docker-entrypoint-initdb.d/mongo-init.js
+++ mongodb/docker-entrypoint-initdb.d/mongo-init.js
@@ -1,5 +1,13 @@
 db.getSiblingDB("$external").runCommand(
     {
+        createUser: "CN=mongo",
+        roles: [
+            {role: "dbOwner", db: "covid_stat"},
+        ],
+    }
+)
+db.getSiblingDB("$external").runCommand(
+    {
         createUser: "CN=scraper",
         roles: [
             {role: "readWrite", db: "covid_stat"},
