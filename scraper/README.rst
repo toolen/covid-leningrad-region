e65fab7b80c8a636744c68e68ef423d8562ac981
Dmitrii Zakharov <dmitrii@zakharov.cc>
2022-03-08 17:57:33 +0300

    docs(scraper): add documentation.

---
+++ scraper/README.rst
@@ -0,0 +1,4 @@
+scraper
+=======================
+
+Web scraper for analyzing the `Rospotrebnadzor page <http://47.rospotrebnadzor.ru/content/%D0%BE%D0%B1%D1%89%D0%B5%D0%B5-%D1%87%D0%B8%D1%81%D0%BB%D0%BE-%D0%B7%D0%B0%D1%80%D0%B5%D0%B3%D0%B8%D1%81%D1%82%D1%80%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D1%8B%D1%85-%D1%81%D0%BB%D1%83%D1%87%D0%B0%D0%B5%D0%B2-covid-2019-%D0%BD%D0%B0-%D1%82%D0%B5%D1%80%D1%80%D0%B8%D1%82%D0%BE%D1%80%D0%B8%D0%B8-%D0%BB%D0%B5%D0%BD%D0%B8%D0%BD%D0%B3%D1%80%D0%B0%D0%B4%D1%81%D0%BA%D0%BE%D0%B9-%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D0%B8-%D1%81-1>`_ with COVID morbidity statistics in the Leningrad region.
--- scraper/scraper/config.py
+++ scraper/scraper/config.py
@@ -1,30 +1,37 @@
 """This file contains configuration properties."""
 import os
+from typing import Dict, Union
 
 from scraper.utils import avoid_empty_value, email_url
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-CERTS_DIR = os.path.join(BASE_DIR, "certs")
+BASE_DIR: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+CERTS_DIR: str = os.path.join(BASE_DIR, "certs")
 
-default_tls_cert_key_path = f"{CERTS_DIR}/scraper.pem"
+default_tls_cert_key_path: str = f"{CERTS_DIR}/scraper.pem"
 if not os.path.exists(default_tls_cert_key_path):
     default_tls_cert_key_path = ""
 
-default_ca_path = f"{CERTS_DIR}/ca.pem"
+default_ca_path: str = f"{CERTS_DIR}/ca.pem"
 if not os.path.exists(default_ca_path):
     default_ca_path = ""
 
-URL = os.getenv("SCRAPER_URL", "http://localhost:8888/page.html")
-DB_URI = os.getenv(
+URL: str = os.getenv("SCRAPER_URL", "http://localhost:8888/page.html")
+DB_URI: str = os.getenv(
     "SCRAPER_DB_URI",
     "mongodb://localhost:27017/covid_stat?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority",
 )
-DB_NAME = os.getenv("SCRAPER_DB_NAME", "covid_stat")
-DB_COLLECTION_NAME = os.getenv("SCRAPER_DB_COLLECTION_NAME", "leningrad_region")
-TLS_CERT_KEY_PATH = os.getenv("SCRAPER_TLS_CERT_KEY_PATH", default_tls_cert_key_path)
-TLS_CA_PATH = os.getenv("SCRAPER_TLS_CA_PATH", default_ca_path)
-EMAIL_URL = email_url(os.getenv("SCRAPER_EMAIL_URL", "smtp://localhost:1025"))
-DEFAULT_FROM_EMAIL = avoid_empty_value(
+DB_NAME: str = os.getenv("SCRAPER_DB_NAME", "covid_stat")
+DB_COLLECTION_NAME: str = os.getenv("SCRAPER_DB_COLLECTION_NAME", "leningrad_region")
+TLS_CERT_KEY_PATH: str = os.getenv(
+    "SCRAPER_TLS_CERT_KEY_PATH", default_tls_cert_key_path
+)
+TLS_CA_PATH: str = os.getenv("SCRAPER_TLS_CA_PATH", default_ca_path)
+EMAIL_URL: Dict[str, Union[str, int, bool]] = email_url(
+    os.getenv("SCRAPER_EMAIL_URL", "smtp://localhost:1025")
+)
+DEFAULT_FROM_EMAIL: str = avoid_empty_value(
     os.getenv("SCRAPER_DEFAULT_FROM_EMAIL", "scraper@local.dev")
 )
-ADMIN_EMAIL = avoid_empty_value(os.getenv("SCRAPER_ADMIN_EMAIL", "admin@local.dev"))
+ADMIN_EMAIL: str = avoid_empty_value(
+    os.getenv("SCRAPER_ADMIN_EMAIL", "admin@local.dev")
+)
